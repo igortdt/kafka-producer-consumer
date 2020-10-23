af2bf6c49156f2e4d11b03720738c693e850d4e5
Igor Bezerra <igortdt@gmail.com>
2020-10-23 14:25:19 -0300

    first commit

---
+++ consume.js
@@ -0,0 +1,19 @@
+const Kafka = require('no-kafka');
+
+// Create an instance of the Kafka consumer
+
+const consumer = new Kafka.SimpleConsumer({"connectionString":"localhost:9092"})
+var data = function (messageSet) {
+    messageSet.forEach(function (m) {
+        var message = m.message.value.toString('utf8');
+    
+        console.log(message);
+        
+    });
+};
+
+// Subscribe to the Kafka topic
+return consumer.init().then(function () {
+    return consumer.subscribe('meu-topico-legal', 0, 
+                         {time: Kafka.EARLIEST_OFFSET}, data);
+});
---
+++ producer.js
@@ -0,0 +1,27 @@
+var kafka = require("kafka-node"),
+Producer = kafka.Producer
+
+client = new kafka.KafkaClient(),
+
+producer = new Producer(client);
+
+
+let count = 0;
+
+producer.on("ready", function() {
+  console.log("ready");
+  setInterval(function() {
+    payloads = [
+      { topic: "meu-topico-legal", messages: `Message ${count}.`, partition: 0 }
+    ];
+
+    producer.send(payloads, function(err, data) {
+      console.log(data);
+      count += 1;
+    });
+  }, 2000);
+});
+
+producer.on("error", function(err) {
+  console.log(err);
+});
